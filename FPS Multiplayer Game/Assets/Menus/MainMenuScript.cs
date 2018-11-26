97e7aed6e5f2ae90c5fbf6c3c11872db34446538
Yernemm <yernemm@yernemm.xyz>
2018-11-26 11:42:40 +0000

    Added menu

---
+++ FPS Multiplayer Game/Assets/Menus/MainMenuScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour {
+
+    public Button btnHost;
+    public Button btnJoin;
+    public InputField inpIp;
+    public InputField inpPort;
+
+	// Use this for initialization
+	void Start () {
+        btnHost.onClick.AddListener(btnHostClick);
+        btnJoin.onClick.AddListener(btnJoinClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void btnHostClick()
+    {
+        Debug.Log("Host Clicked");
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+
+    void btnJoinClick()
+    {
+        Debug.Log("Join Clicked");
+    }
+}
