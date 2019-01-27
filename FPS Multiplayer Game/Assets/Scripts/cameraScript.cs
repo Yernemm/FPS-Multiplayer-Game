b3194607664aaa4a41c086dd4c2053652deb4f89
Yernemm <yernemm@yernemm.xyz>
2019-01-27 22:44:10 +0000

    sure

--- FPS Multiplayer Game/Assets/Scripts/cameraScript.cs
+++ FPS Multiplayer Game/Assets/Scripts/cameraScript.cs
@@ -1,15 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
 
-public class cameraScript : NetworkBehaviour {
+public class cameraScript : MonoBehaviour {
 
     
     //Player rigid body will be set in Unity.
    // public Rigidbody player;
     Vector3 posOffset;
-    [SerializeField]
     GameObject player;
     UIScript ui;    
 
@@ -26,25 +24,16 @@ public class cameraScript : NetworkBehaviour {
             g.GetComponent<MeshRenderer>().enabled = false;
         }
     }
-
-  
     public void initPlayerCamera()
     {
-        //GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-        //foreach (GameObject p in players)
-        //{
-        //    if (p.GetComponent<playerController>().localPlayer)
-        //    {
-        //        player = p;
-        //    }
-        //}
-
-       // if (player.GetComponent<playerController>().localPlayer)
-      //  {
-           // player = transform.parent.gameObject;
-       // }
-      //  else
-       //     gameObject.SetActive(false);
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject p in players)
+        {
+            if (p.GetComponent<playerController>().localPlayer)
+            {
+                player = p;
+            }
+        } 
     }
 	
 	// Update is called once per frame
@@ -69,7 +58,7 @@ public class cameraScript : NetworkBehaviour {
 
     private void LateUpdate()
     {
-       // transform.position = player.GetComponent<Rigidbody>().position + posOffset;
+        transform.position = player.GetComponent<Rigidbody>().position + posOffset;
         //transform.eulerAngles = player.transform.eulerAngles;
     }
 }
