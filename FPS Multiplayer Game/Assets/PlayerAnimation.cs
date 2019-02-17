be10c9c34998b464d2d15340f01a2c48cb6abcb9
Yernemm <yernemm@yernemm.xyz>
2019-02-17 18:30:23 +0000

    It walks now

---
+++ FPS Multiplayer Game/Assets/PlayerAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField]
+    Animator anim;
+    [SerializeField]
+    string walkAnim = "Walk";
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            anim.Play(walkAnim, 0, 0);
+        }
+    }
+}
